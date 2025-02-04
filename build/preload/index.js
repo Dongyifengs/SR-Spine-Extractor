acd202e87b8c196fa301d434e435283409342919
MoYi <1545929126@qq.com>
2025-02-04 20:51:06 +0800

    ci: remove the build dir

--- build/preload/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('demo', {
-    count: (count) => {
-        electron_1.ipcRenderer.send("count", count);
-    }
-});
