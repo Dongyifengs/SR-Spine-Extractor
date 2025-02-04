9d2b88fccc7caa14d2888258fd1ab2aa115609f4
MoYi <1545929126@qq.com>
2025-02-05 01:18:34 +0800

    feat: Auto Element

--- src/vue/components.d.ts
+++ src/vue/components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElLink: typeof import('element-plus/es')['ElLink']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElSelect: typeof import('element-plus/es')['ElSelect']
     ElText: typeof import('element-plus/es')['ElText']
